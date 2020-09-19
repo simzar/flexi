31c3b28762b16a1864ccc51878d67055ac18fb28
simonas <simonas.zaronskis@devbridge.com>
2020-09-19 17:02:00 +0300

    Add material ui and example usage

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const PrettyButton = (props) => (
+    <Button variant={props.variant} color={props.color}>
+        {props.content}
+    </Button>
+);
+
+PrettyButton.defaultProps = {
+    variant: 'contained',
+    color: 'primary',
+    content: 'I am pretty',
+}
+
+export default PrettyButton;
