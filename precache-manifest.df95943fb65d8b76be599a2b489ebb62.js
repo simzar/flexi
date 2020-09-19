a7a14d002bf3b6ac94eba43c92f91e2b7e3fdaed
simonas <simonas.zaronskis@devbridge.com>
2020-09-19 17:31:06 +0300

    Updates

---
+++ precache-manifest.df95943fb65d8b76be599a2b489ebb62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74206b1abbeccb8a93bd670cce7fb8fd",
+    "url": "/flexitis/index.html"
+  },
+  {
+    "revision": "e25ea682da6221a34c30",
+    "url": "/flexitis/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "8d807fb4c064dba331b8",
+    "url": "/flexitis/static/js/2.3f682a3d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flexitis/static/js/2.3f682a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25ea682da6221a34c30",
+    "url": "/flexitis/static/js/main.09a522ee.chunk.js"
+  },
+  {
+    "revision": "7a58a5d60e04c6ccb9c0",
+    "url": "/flexitis/static/js/runtime-main.9602f5b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flexitis/static/media/logo.5d5d9eef.svg"
+  }
+]);
