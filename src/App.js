4adf4cac5a469088521000b34a97e68f7f25195b
simonas <simonas.zaronskis@devbridge.com>
2020-09-19 18:45:39 +0300

    Add weird menu component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import PrettyButton from "./components/Button";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PrettyButton from './components/Button';
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <Menu />
+        <img src={logo} className='App-logo' alt='logo' />
         <PrettyButton />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
