0290ea60a24f2cff4c74459a1acbaa5b8629d905
simonas <simonas.zaronskis@devbridge.com>
2020-09-20 10:36:29 +0300

    Expose methods for displaying notifications

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Wallet from './containers/Wallet';
 import Prizes from './containers/Prizes';
 
 import './App.css';
+import { showNotification } from './utils/notificationUtils';
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,5 +27,15 @@ const App = () => {
     </div>
   );
 };
+
+window.showReduceConsumptionToast = () =>
+  showNotification(
+    'If you reduced your electricity consumption for the next hour, you could receive up to 10 additional flexiCoins!',
+  );
+window.showIncreaseConsumptionToast = () =>
+  showNotification(
+    'If you increased your electricity consumption for the next hour, you could receive up to 8 additional flexiCoins!',
+  );
+window.showToast = (message = 'a toast') => showNotification(message);
 
 export default App;
