b8d8f9be0a3fe080f948940ba4cb586429f3fb44
simonas <simonas.zaronskis@devbridge.com>
2020-09-20 09:28:27 +0300

    Clean up a little

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 
-// import {
-//   Chart,
-//   ArgumentAxis,
-//   ValueAxis,
-//   Title,
-//   BarSeries,
-//   LineSeries,
-// } from '@devexpress/dx-react-chart-material-ui';
-// import { Animation } from '@devexpress/dx-react-chart';
-
 import {
   Chart,
   Series,
@@ -30,43 +20,25 @@ import {
 const LineChart = ({ data }) => {
   return (
     <Paper>
-      <Chart palette='Harmony Light' title='Population: Age Structure (2000)' dataSource={data}>
+      <Chart palette='Harmony Light' title='FlexiCoin kainos istorija' dataSource={data}>
         <CommonSeriesSettings type={'area'}>
           <Point visible color='#F99360' hoverMode='allArgumentPoints' />
           <Label visible={true}>
             <Connector visible={true} />
           </Label>
-          d
         </CommonSeriesSettings>
         <Series
           argumentField='hour'
           valueField='price'
-          name='FlexiCoin tarifas uz 1kWh'
+          name='FlexiCoin tarifas už 1kWh'
           color='#ffd700'
         />
         <Margin bottom={20} />
         <ArgumentAxis valueMarginsEnabled={false} />
         <Legend verticalAlignment='bottom' horizontalAlignment='center' />
-        <Export enabled />
       </Chart>
     </Paper>
   );
 };
-
-//
-// const LineChart = React.memo(({ data }) => {
-//   data = [...data, { price: 12, hour: '21:00' }];
-//   return (
-//     <Paper>
-//       <Chart data={data}>
-//         <ArgumentAxis />
-//         <ValueAxis max={10} />
-//         <LineSeries valueField='price' argumentField='hour' />
-//         <Title text='FlexiCoin kainos istorija' />
-//         <Animation />
-//       </Chart>
-//     </Paper>
-//   );
-// });
 
 export default LineChart;
