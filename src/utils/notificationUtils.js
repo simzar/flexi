d68d6e63f95ac7cf62a550dbd19c938e9c8b91d6
simonas <simonas.zaronskis@devbridge.com>
2020-09-20 13:42:31 +0300

    Fix mistypes

--- src/utils/notificationUtils.js
+++ src/utils/notificationUtils.js
@@ -2,7 +2,7 @@ export function showNotification(message) {
   Notification.requestPermission(function (result) {
     if (result === 'granted') {
       navigator.serviceWorker.ready.then(function (registration) {
-        registration.showNotification('Flexiti has some good news!', {
+        registration.showNotification('Flexitis has some good news!', {
           body: message,
           icon: './flexitis_logo_64.png',
           vibrate: [200, 100, 200, 100, 200, 100, 200],
